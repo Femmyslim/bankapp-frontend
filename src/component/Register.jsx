82f93467c58c96009d6b721094dbf43c4bd90524
unknown <femisolaalawiye@gmail.com>
2022-11-23 02:51:05 +0100

    cloning of kuda sign in page

---
+++ src/component/Register.jsx
@@ -0,0 +1,74 @@
+// import React from "react";
+// import "../css/App.css"
+
+
+// // function Register (){
+
+
+// //     return(
+
+
+// //         <div>
+// //             <h></h>
+// //         </div>
+// //     )
+// // }
+
+// // export default Register
+
+
+// // export default function Register (){
+
+
+// //     return(
+
+
+// //         <div>
+// //             <h></h>
+// //         </div>
+// //     )
+// // }
+
+// const Register  = () => {
+
+//     const submitForm =() =>  {
+
+//         alert(`Detail submitted correctly`)
+//     }
+
+
+
+//         return(
+//                     <div>
+//                         <h1 className="text" >Welcome to react first class</h1>
+//                         <form action="">
+//                             <label htmlFor="Lastname">Lastname</label>
+//                             <input type="text" id="Lastname" placeholder="Type in your lastname" required/>
+//                             <br />
+//                             <br />
+//                             <label htmlFor="Othernames">Othernames</label>
+//                             <input type="text" id="Othernames" placeholder="Type in your othernames" required/>
+//                             <br />
+//                             <br />
+//                             <label htmlFor="Email">Email</label>
+//                             <input type="email" id="Email" placeholder="Input your email@gmail.com" required />
+//                             <br />
+//                             <br />
+//                             <p>Gender:</p>
+//                             <label htmlFor="Female">Female</label>
+//                             <input type="radio" name="click" id="Female" />
+//                             <label htmlFor="Male">Male</label>
+//                             <input type="radio" name="click" id="Male" />
+//                             <label htmlFor="Undecided">Undecided</label>
+//                             <input type="radio" name="click" id="Undecided" />
+//                             <br />
+//                             <br />
+//                             <button type="submit" onClick={submitForm}>Submit</button>
+
+//                         </form>
+//                     </div>
+                
+//     )
+
+// }
+// export default Register
